1f223d4db58ee06f792bdb6c18ef4be55d4d6bf2
Roger Lo <sobadrush@icloud.com>
2022-06-01 20:11:29 +0800

    initial commit: 1. 建立SpringBoot專案 2. 完成內部H2 Database環境搭建

---
+++ src/main/java/com/cathay/springbootswaggertest/SpringBootSwaggerTestApplication.java
@@ -0,0 +1,13 @@
+package com.cathay.springbootswaggertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwaggerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSwaggerTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cathay/springbootswaggertest/SpringBootSwaggerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cathay.springbootswaggertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSwaggerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
