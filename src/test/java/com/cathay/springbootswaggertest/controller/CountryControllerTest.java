3baa9c016b6b487b990c1f215f47c75e52a9fac3
Roger <sobadrush@icloud.com>
2022-06-04 18:33:25 +0800

    CountryControllerTest 1. 使用 randomServerPort 2. 使用 contextPath 3. 使用 postForEntity 測試新增

--- src/test/java/com/cathay/springbootswaggertest/controller/CountryControllerTest.java
+++ src/test/java/com/cathay/springbootswaggertest/controller/CountryControllerTest.java
@@ -5,13 +5,20 @@ import com.cathay.springbootswaggertest.model.CountryVO;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author RogerLo
@@ -24,14 +31,24 @@ public class CountryControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Value("${server.servlet.context-path}")
+    private String contextPath; // should populate with /app-service
+
+    @LocalServerPort // 使用隨機PORT → 需搭配 SpringBootTest.WebEnvironment.RANDOM_PORT 使用(ref. https://howtodoinjava.com/spring-boot2/testing/testresttemplate-post-example/)
+    private int randomServerPort;
+
     @Test // 參考：https://mkyong.com/spring-boot/spring-boot-hello-world-example/
     @Disabled
     public void test001() throws Exception {
+        final String endPointUrl = "http://localhost:"
+                                    + randomServerPort
+                                    + contextPath + "/CountryController/country/getAll";
+
         // 取得 ResponseEntity<集合形式>
         // ref. https://java.tutorialink.com/how-to-get-list-from-object-in-spring-resttemplate/
         ResponseEntity<Set<CountryVO>> responseEntity =
                 restTemplate.exchange(
-                "http://localhost:8080/RogerSpringBoot/CountryController/country/getAll",
+                endPointUrl,
                 HttpMethod.GET, null,
                 new ParameterizedTypeReference<Set<CountryVO>>() {}
         );
@@ -41,4 +58,22 @@ public class CountryControllerTest {
                 responseEntity.getBody().size()).isGreaterThan(1);
     }
 
+    @Test
+    @Disabled
+    public void test002() {
+        final String endPointUrl = "http://localhost:"
+                                + randomServerPort
+                                + contextPath + "/CountryController/country";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Stream.of(MediaType.APPLICATION_JSON).collect(Collectors.toList()));
+
+        CountryVO countryForInsert = CountryVO.builder().countryName("Gambia").build(); // 甘比亞
+        HttpEntity<CountryVO> request = new HttpEntity<>(countryForInsert, headers);
+
+        // 使用 postForEntity (ref. https://howtodoinjava.com/spring-boot2/testing/testresttemplate-post-example/)
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(endPointUrl, request, String.class);
+        System.err.println("CountryControllerTest.test002 responseEntity = " + responseEntity);
+    }
 }
